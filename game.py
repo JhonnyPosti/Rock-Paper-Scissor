a256c426df0427dd76972bd7c073e388caba0e38
JhonnyPosti <joaogabriel975@hotmail.com>
2022-04-06 23:43:04 -0300

    maxter

--- game.py
+++ game.py
@@ -1,19 +1,33 @@
 import random
 import PySimpleGUI as sg
 
-
 decisions = {
     '0' : 'Rock',
     '1' : 'Paper',
     '2' : 'Scissor'
 }
 
-first_collumn = [
-    [
-        sg.Text("Escolha sua opção"),
-        sg.Button("Rock"),
-        sg.Button("Paper"),
-        sg.Button("Scissor")
-    
+def robotplay():
+    robotchoice = random.choice(list(decisions))
+    return robotchoice
+
+def main():
+    radio_buttons = ['Rock', 'Paper', 'Scissor']
+    layout = [
+        [sg.Text('Selecione uma jogada')],
+        [sg.Radio(text, 1) for text in radio_buttons],
+        [sg.Button('Read')]
     ]
-]
+
+    window = sg.Window('Rock Paper & Scissor', layout)
+
+    while True:
+        event, values = window.Read()
+        robotchoice = robotplay()
+        if event is None:
+            break
+        print(event, values)
+        print(robotchoice)
+
+if __name__ == "__main__":
+    main()
